15818a5dc71181a1396e41bd0728ad3abf5d0bb2
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-07-30 13:46:15 +0530

    ui fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [students, setStudents] = useState([]);
   const [selectedStudents, setSelectedStudents] = useState([]);
   const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
-  const [startDate, setStartDate] = useState(new Date(new Date().getFullYear(), new Date().getMonth(), 1).toISOString().split('T')[0]); 
+  const [startDate, setStartDate] = useState(new Date(new Date().getFullYear(), new Date().getMonth(), 1).toISOString().split('T')[0]);
   const [endDate, setEndDate] = useState(new Date().toISOString().split('T')[0]);
   const [showConfirmation, setShowConfirmation] = useState(false);
 
@@ -113,88 +113,100 @@ function App() {
   };
 
   return (
-    <main className='w-full h-[88svh] grid place-items-center'>
-      <div className='grid place-items-center gap-5 text-white p-10 border border-slate-100 rounded-md min-h-[25%]'>
-
-        <input
-          type="date"
-          value={date}
-          onChange={handleDateChange}
-          className='bg-transparent border border-slate-400 rounded-lg px-2'
-        />
-        <select value={selectedCourse} onChange={handleCourseChange} className='bg-transparent border border-slate-400 rounded-lg'>
-          <option value="" className='text-black'>--Select Course--</option>
-          {courses.map((course, index) => (
-            <option key={index} value={course} className='text-black'>{course}</option>
-          ))}
-        </select>
-
-        {selectedCourse && (
-          <select value={selectedTeacher} onChange={handleTeacherChange} className='bg-transparent border border-slate-400 rounded-lg'>
-            <option value="" className='text-black'>--Select Teacher--</option>
-            {teachers.map((teacher, index) => (
-              <option key={index} value={teacher} className='text-black'>{teacher}</option>
-            ))}
-          </select>
-        )}
-
-        {selectedTeacher && (
-          <div>
-            <h3>Students:</h3>
-            <ul>
-              {students.map((student, index) => (
-                <li key={index}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      value={student}
-                      onChange={handleStudentChange}
-                      checked={selectedStudents.includes(student)}
-                    />
-                    {student}
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-        {selectedCourse && selectedTeacher && (
-          <div>
-            <button type='button' className='bg-mybl px-4 py-2 rounded-lg' onClick={handleSubmit}>SUBMIT</button>
-          </div>
-        )}
-
-        <div>
+    <>
+      <header className=' w-full text-white flex justify-center items-center gap-5 mt-5 p-4 relative bg-black bg-opacity-35'>
+        <div className=' relative w-fit'> <label htmlFor="from">From :</label>
           <input
+            id='from'
             type="date"
             value={startDate}
             onChange={(e) => setStartDate(e.target.value)}
-            className='bg-transparent border border-slate-400 rounded-lg px-2'
-          />
+            className='bg-transparent border border-slate-400 rounded-lg px-2 w-fit'
+          /></div>
+        <div className=' relative w-fit'>
+          <label htmlFor="to">To :</label>
           <input
+            id='to'
             type="date"
             value={endDate}
             onChange={(e) => setEndDate(e.target.value)}
-            className='bg-transparent border border-slate-400 rounded-lg px-2'
-          />
-          <button type='button' className='bg-mybl px-4 py-2 rounded-lg' onClick={handleDownloadLogs}>Download Logs as Excel</button>
-        </div>
-
-        {showConfirmation && (
-          <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
-            <div className='bg-white p-5 rounded-lg text-black'>
-              <h3 className='mb-4'>Confirm Download</h3>
-              <p>Are you sure you want to download logs between {startDate} and {endDate}?</p>
-              <div className='mt-4 flex justify-end gap-2'>
-                <button type='button' className='bg-mybl px-4 py-2 rounded-lg' onClick={confirmDownload}>Confirm</button>
-                <button type='button' className='bg-red-500 px-4 py-2 rounded-lg' onClick={handleCancel}>Cancel</button>
-              </div>
+            className='bg-transparent border border-slate-400 rounded-lg px-2 w-fit'
+          /></div>
+        <button type='button' className='bg-mybl px-4 py-2 rounded-lg block' onClick={handleDownloadLogs}>Excel</button>
+      </header>
+      {showConfirmation && (
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
+          <div className='bg-white p-5 rounded-lg text-black'>
+            <h3 className='mb-4'>Confirm Download</h3>
+            <p>Are you sure you want to download logs between {startDate} and {endDate}?</p>
+            <div className='mt-4 flex justify-end gap-2'>
+              <button type='button' className='bg-mybl px-4 py-2 rounded-lg' onClick={confirmDownload}>Confirm</button>
+              <button type='button' className='bg-red-500 px-4 py-2 rounded-lg' onClick={handleCancel}>Cancel</button>
             </div>
           </div>
-        )}
-      </div>
-    </main>
+        </div>
+      )}
+      <main className='w-full mt-10 relative grid place-items-center'>
+        <div className='grid place-items-start gap-5 text-white p-10 rounded-md min-h-[25%]'>
+          <div>
+            <label htmlFor="date">Date:</label>
+            <input
+              id='date'
+              type="date"
+              value={date}
+              onChange={handleDateChange}
+              className='bg-transparent border border-slate-400 rounded-lg px-1'
+            /></div>
+          <div>
+            <label htmlFor="course">Course:</label>
+            <select id="course" value={selectedCourse} onChange={handleCourseChange} className='bg-transparent border border-slate-400 rounded-lg'>
+              <option value="" className='text-black'>--Select Course--</option>
+              {courses.map((course, index) => (
+                <option key={index} value={course} className='text-black'>{course}</option>
+              ))}
+            </select>
+          </div>
+
+          {selectedCourse && (
+            <div>
+               <label htmlFor="teachers">Teachers:</label>
+              <select id="teachers" value={selectedTeacher} onChange={handleTeacherChange} className='bg-transparent border border-slate-400 rounded-lg'>
+                <option value="" className='text-black'>--Select Teacher--</option>
+                {teachers.map((teacher, index) => (
+                  <option key={index} value={teacher} className='text-black'>{teacher}</option>
+                ))}
+              </select>
+            </div>
+          )}
+
+          {selectedTeacher && (
+            <div>
+              <h3>Students:</h3>
+              <ul>
+                {students.map((student, index) => (
+                  <li key={index}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        value={student}
+                        onChange={handleStudentChange}
+                        checked={selectedStudents.includes(student)}
+                      />
+                      {student}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {selectedCourse && selectedTeacher && (
+            <div>
+              <button type='button' className='bg-mybl px-4 py-2 rounded-lg' onClick={handleSubmit}>SUBMIT</button>
+            </div>
+          )}
+        </div>
+      </main></>
   );
 }
 
